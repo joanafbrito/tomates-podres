442baabdcc81d84ab2707a1650953aa321c558a7
Joana Brito <joanafiorentini@hotmail.com>
2021-08-25 20:23:34 -0700

    Add work and create basic structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import movieData from './movieData';
+import React, { Component }from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: [],
+      error: ''
+    }
+  }
+
+  compontentDidMount() {
+    this.setState({
+      movies: [...this.state.movies, ...movieData]
+      // fetch call with data info
+    })    
+  }
+
+  render() {
+  return ( <h2>Hello</h2>
+
+
+    // Nav Bar - Search button(input)
+    // Movies Components- all movies
+    // MovieCard Image
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+    );  
+  } 
+}
 export default App;
