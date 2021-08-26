3354aa7db3c196868d8a01750e8a6549a917ab8b
Joana Brito <joanafiorentini@hotmail.com>
2021-08-25 21:48:23 -0700

    Delete problematic line

--- src/MovieCard.js
+++ src/MovieCard.js
@@ -4,7 +4,6 @@ import './MovieCard.css';
 const MovieCard = (props) => {
     return (
         <img src={props.poster} className="poster" alt="Movie Poster" />
-        {/* <img>{props.poster}</img> */}
     )
 }
 
