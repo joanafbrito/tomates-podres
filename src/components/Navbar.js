548d61c14c5b1d1613d7749581d2ae255af2b5fa
Megan McBride <megan.mcbride@colorado.edu>
2021-09-01 15:13:35 -0600

    Adding Joana's amazing feature of the greeting by time of day

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,15 +6,29 @@ import logo from '../images/tomato1.svg';
 
 
 const Navbar = ({isSearchBar, filterMovies, updateSearchBar}) => {
+  const date = new Date()
+  const hours = date.getHours()
+  let timeOfDay;
+
+  if (hours < 12) {
+    timeOfDay = 'Morning'
+  } else if (hours >= 12 && hours < 17) {
+    timeOfDay = 'Afternoon'
+  } else {
+    timeOfDay = 'Night'
+  }
   return (
     <div className='navbar'>
       <div className='logo-name'>
         <img className='logo'src={logo} alt='Logo-Tomato' />
         <h1>Tomates Podres</h1>
       </div>
-      {isSearchBar && 
-      <SearchBar filterMovies={filterMovies}/>}
-      {!isSearchBar && <Link to='/'><button onClick={() => updateSearchBar(true)}>Return to Home</button></Link>}
+      <div className='timeGreetings'>
+        <h5>Good {timeOfDay}!</h5>
+        {isSearchBar && 
+        <SearchBar filterMovies={filterMovies}/>}
+        {!isSearchBar && <Link to='/'><button onClick={() => updateSearchBar(true)}>Return to Home</button></Link>}
+      </div>      
     </div>
   )
 }
